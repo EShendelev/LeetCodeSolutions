36512d3f9e63e0bbb9447774e3ef0e943b50390c
EShendelev <sendeleve@yandex.ru>
2022-06-15 21:12:17 +0300

    LeetCode 2236 solution

---
+++ 2236_RootEqualsSumofChildren/main.go
@@ -0,0 +1,15 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	//
+}
+
+func checkTree(root *TreeNode) bool {
+	return root.Val == root.Left.Val+root.Right.Val
+}
